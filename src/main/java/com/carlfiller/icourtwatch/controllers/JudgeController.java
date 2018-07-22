d6afb3084c9db5223c65a1914eba78556b6279eb
cfiller123 <carlfiller@gmail.com>
2018-07-22 14:25:43 -0500

    Working on initial add court watch form.

--- src/main/java/com/carlfiller/icourtwatch/controllers/JudgeController.java
+++ src/main/java/com/carlfiller/icourtwatch/controllers/JudgeController.java
@@ -1,8 +1,10 @@
 package com.carlfiller.icourtwatch.controllers;
 
+import com.carlfiller.icourtwatch.models.Judge;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("judge")
@@ -14,5 +16,13 @@ public class JudgeController {
         model.addAttribute("title","Welcome to CourtWatch!");
 
         return "judge/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddWatchForm(Model model) {
+
+        model.addAttribute("title","Add Your Court Watch");
+        model.addAttribute(new Judge());
+        return "judge/addwatch";
     }
 }
--- src/main/java/com/carlfiller/icourtwatch/models/Judge.java
+++ src/main/java/com/carlfiller/icourtwatch/models/Judge.java
@@ -4,13 +4,40 @@ import javax.validation.constraints.NotNull;
 
 public class Judge {
 
-    @NotNull
     private String name;
-    @NotNull
     private String court;
-    @NotNull
     private String defendant;
-    @NotNull
     private Disposition disposition;
 
+    public String getName() {
+        return name;
+    }
+
+    public String getCourt() {
+        return court;
+    }
+
+    public String getDefendant() {
+        return defendant;
+    }
+
+    public Disposition getDisposition() {
+        return disposition;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCourt(String court) {
+        this.court = court;
+    }
+
+    public void setDefendant(String defendant) {
+        this.defendant = defendant;
+    }
+
+    public void setDisposition(Disposition disposition) {
+        this.disposition = disposition;
+    }
 }
