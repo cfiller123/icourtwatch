b3a9bdcbc37f40763d9138b8580030a1567972f9
cfiller123 <carlfiller@gmail.com>
2018-07-23 13:22:24 -0500

    Working on creating user accounts.

--- src/main/java/com/carlfiller/icourtwatch/controllers/UserController.java
+++ src/main/java/com/carlfiller/icourtwatch/controllers/UserController.java
@@ -3,8 +3,12 @@ package com.carlfiller.icourtwatch.controllers;
 import com.carlfiller.icourtwatch.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
@@ -21,5 +25,16 @@ public class UserController {
         model.addAttribute(new User());
         model.addAttribute("title", "Register Your Account");
         return "user/signup";
+    }
+
+    @RequestMapping(value = "signup", method = RequestMethod.POST)
+    public String processUserSignUpForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
+        model.addAttribute(user);
+
+        if (!errors.hasErrors()) {
+            return "user/signup";
+        }
+
+        return "judge/index";
     }
 }
