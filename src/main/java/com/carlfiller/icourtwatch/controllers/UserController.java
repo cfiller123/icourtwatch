2d213df42d4bc76d2875fe8ba8c945c7fe569902
cfiller123 <carlfiller@gmail.com>
2018-07-24 19:25:31 -0500

    Set up cookies to save by username. Now to check if it exists and welcome user by name.

--- src/main/java/com/carlfiller/icourtwatch/controllers/UserController.java
+++ src/main/java/com/carlfiller/icourtwatch/controllers/UserController.java
@@ -9,7 +9,9 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Controller
@@ -20,7 +22,9 @@ public class UserController {
     private UserDao userDao;
 
     @RequestMapping(value = "index", method = RequestMethod.GET)
-    public String index(Model model) {
+    public String index(Model model, HttpServletRequest request) {
+//        request.getCookies();
+//        TODO: check if a "user" cookie exists; if not, send to login page. If exists, welcome user by name.
         model.addAttribute("title", "User Management Page");
         return "user/index";
     }
@@ -33,15 +37,28 @@ public class UserController {
     }
 
     @RequestMapping(value = "signup", method = RequestMethod.POST)
-    public String processUserSignUpForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
+    public String processUserSignUpForm(Model model, @ModelAttribute @Valid User user, Errors errors, HttpServletResponse response) {
         model.addAttribute(user);
 
         if (!errors.hasErrors()) {
             userDao.save(user);
+            Cookie logincookie = new Cookie("user", user.getUsername());
+            response.addCookie(logincookie);
             return "judge/index";
         }
 
         model.addAttribute("title", "Register Your Account");
         return "user/signup";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String displayUserLoginForm(Model model) {
+        model.addAttribute("title", "Login");
+        return "user/login";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String processUserLoginForm(Model model, @ModelAttribute @Valid User user, Errors errors, HttpServletResponse response) {
+        return "redirect:";
     }
 }
