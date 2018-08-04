b67f52a59cbfcc74acdbae6aeb71846ed6b3cdfb
cfiller123 <carlfiller@gmail.com>
2018-08-04 17:03:14 -0500

    Working on adding dates to watches.

--- src/main/java/com/carlfiller/icourtwatch/controllers/JudgeController.java
+++ src/main/java/com/carlfiller/icourtwatch/controllers/JudgeController.java
@@ -2,17 +2,13 @@ package com.carlfiller.icourtwatch.controllers;
 
 import com.carlfiller.icourtwatch.models.Disposition;
 import com.carlfiller.icourtwatch.models.Judge;
-import com.carlfiller.icourtwatch.models.data.JudgeDao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import javax.validation.Valid;
-import java.util.ArrayList;
 
 @Controller
 @RequestMapping("judge")
