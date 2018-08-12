9333d03e202aef291bf138bc599d817027edc479
cfiller123 <carlfiller@gmail.com>
2018-08-12 09:50:22 -0500

    Trying to fix canvas chart bean error.

--- src/main/java/com/carlfiller/icourtwatch/controllers/CanvasjsChartController.java
+++ src/main/java/com/carlfiller/icourtwatch/controllers/CanvasjsChartController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("data")
 public class CanvasjsChartController extends AbstractController{
 
-    @Autowired(required = false) //Take off false to see error.
+    @Autowired
     private CanvasjsChartService canvasjsChartService;
 
     @RequestMapping(method = RequestMethod.GET)
