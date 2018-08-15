30994b4c03204aed9cc5bd2885c59cd362411ec0
cfiller123 <carlfiller@gmail.com>
2018-08-14 19:00:38 -0500

    Chart works!

--- src/main/java/com/carlfiller/icourtwatch/controllers/DataController.java
+++ src/main/java/com/carlfiller/icourtwatch/controllers/DataController.java
@@ -1,10 +1,13 @@
 package com.carlfiller.icourtwatch.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import com.carlfiller.icourtwatch.models.Judge;
 import com.carlfiller.icourtwatch.models.User;
+import com.carlfiller.icourtwatch.models.Watch;
 import com.carlfiller.icourtwatch.models.service.CanvasjsChartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,6 +25,43 @@ public class DataController extends AbstractController{
     @Autowired
     private CanvasjsChartService canvasjsChartService;
 
+    public List<Integer> getAverages(Judge foundJudge) {
+        List<Watch> watches = watchDao.findByJudge(foundJudge);
+        int audability = 0;
+        int caseDetails = 0;
+        int courtProceedings = 0;
+        int explainCharges = 0;
+        int eyeContact = 0;
+        int listeningSkills = 0;
+        int timeMgmt = 0;
+        int voiceTone = 0;
+        for (Watch w : watches) {
+            audability = audability + w.getAudability();
+            caseDetails = caseDetails + w.getCaseDetails();
+            courtProceedings = courtProceedings + w.getCourtProceedings();
+            explainCharges = explainCharges + w.getExplainCharges();
+            eyeContact = eyeContact + w.getEyeContact();
+            listeningSkills = listeningSkills + w.getListeningSkills();
+            timeMgmt = timeMgmt + w.getTimeMgmt();
+            voiceTone = voiceTone + w.getVoiceTone();
+        }
+        audability = audability/watches.size();
+        caseDetails = caseDetails/watches.size();
+        courtProceedings = courtProceedings/watches.size();
+        explainCharges = explainCharges/watches.size();
+        eyeContact = eyeContact/watches.size();
+        listeningSkills = listeningSkills/watches.size();
+        timeMgmt = timeMgmt/watches.size();
+        voiceTone = voiceTone/watches.size();
+        List<Integer> data = new ArrayList<>();
+        data.addAll(Arrays.asList(audability,caseDetails,courtProceedings,explainCharges, eyeContact,listeningSkills
+        , timeMgmt, voiceTone));
+        return data;
+//        return String.valueOf(audability) + ", " + String.valueOf(caseDetails) + ", " + String.valueOf(courtProceedings)
+//                + ", " + String.valueOf(explainCharges) + ", " + String.valueOf(eyeContact) + ", " + String.valueOf(listeningSkills)
+//                + ", " + String.valueOf(timeMgmt) + ", " + String.valueOf(voiceTone);
+    }
+
     @RequestMapping(value = "index", method = RequestMethod.GET)
     public String displaySummary(Model model, HttpServletRequest request) {
         User user = getUserFromSession(request.getSession());
@@ -29,6 +69,7 @@ public class DataController extends AbstractController{
 
         model.addAttribute("title", "Summary Statistics");
         model.addAttribute("watches",watchDao.findAll().size());
+        model.addAttribute("judges",judgeDao.findAll());
         model.addAttribute("yourname",name);
 
         return "data/index";
@@ -43,9 +84,12 @@ public class DataController extends AbstractController{
     }
 
     @RequestMapping(value="highchart",method = RequestMethod.GET)
-    public String highchart(Model model) {
-        model.addAttribute("chartTitle","Test");
+    public String highchart(Model model, int id) {
+        Judge foundjudge = judgeDao.findOne(id);
+        model.addAttribute("title","Summary Statistics");
+        model.addAttribute("chartTitle",foundjudge.getName() + "'s Results");
         model.addAttribute("chartScale","Scale from 1-5 with 1 being the lowest");
+        model.addAttribute("data",getAverages(foundjudge)); //need to pass as integers not string
         model.addAttribute("chartMetric","Eye Contact");
         return "data/highchart";
     }
--- src/main/java/com/carlfiller/icourtwatch/models/data/WatchDao.java
+++ src/main/java/com/carlfiller/icourtwatch/models/data/WatchDao.java
@@ -1,5 +1,6 @@
 package com.carlfiller.icourtwatch.models.data;
 
+import com.carlfiller.icourtwatch.models.Judge;
 import com.carlfiller.icourtwatch.models.Watch;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,8 @@ public interface WatchDao extends CrudRepository<Watch, Integer> {
 
     @Override
     List<Watch> findAll();
+
+    List<Watch> findByJudge(Judge judge);
 
     List<Watch> findByOwnerId(int ownerId);
 
